440c153c1eb0b790f1c0f66d69054a621a18b603
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-19 18:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 var isDate = function (input) {
   //   write your code here
-};
-
+	if(input == "new Date()" || input="new Date(year,month,day)"){
+		return true;
+	}
+	else{
+		return false;  
+	}
+};   
+ 
 // Do not change the code below.
+
 const input = prompt("Enter Date.");
-alert(isDate(input));
+alert(isDate(input)); 
+ 
